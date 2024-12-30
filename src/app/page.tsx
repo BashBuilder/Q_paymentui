8a57c747ab704c75c689313ed3b5a27424aa7ede
unknown <iamadelowotan@gmail.com>
2024-12-30 11:16:06 +0100

    updata: naming convention updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="flex h-screen items-center justify-center text-center">
-      <h1>Qorepay payment ui</h1>
+    <div className="flex h-screen flex-col items-center justify-center space-y-6">
+      <div className="text-center">
+        <h1 className="text-4xl font-semibold">Payment test links</h1>
+      </div>
+
+      <section className="flex gap-4">
+        <a href="/purchase" className="flex flex-col gap-3 text-center">
+          <p>Payment link</p>
+          <div>
+            <Button>Purchase</Button>
+          </div>
+        </a>
+        <a href="/purchase" className="flex flex-col gap-3 text-center">
+          <p>Recurring payment</p>
+          <div>
+            <Button>Purchase</Button>
+          </div>
+        </a>
+      </section>
     </div>
   );
 }
---
+++ src/app/purchase/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="min-h-screen bg-slate-200">
+      <div className="flex min-h-screen items-center justify-center">
+        <section className="rounded-lg bg-white p-10">
+          <p>Payment link</p>
+          <p>Example loaded </p>
+          <p>Payment link</p>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default page;
